f2a7b805e8d4a212a8b40f93f37bc5af03822b13
V-SPOT <83084169+ajmalyaseen@users.noreply.github.com>
2021-05-12 13:53:53 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -9,9 +9,13 @@ from bot.database import Database # pylint: disable=import-error
 
 db = Database()
 
-@Client.on_message(filters.command(["start"]) & filters.private, group=1)
-async def start(bot, update):
-    update_channel = 'filmzoneeee'
+pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
+async def echo(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
+        return
+    TRChatBase(update.from_user.id, update.text, "/echo")
+    update_channel = 'filmzoneeee
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
@@ -19,16 +23,13 @@ async def start(bot, update):
                await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
                return
         except UserNotParticipant:
-            #await update.reply_text(f"Join my channel To Use Me")
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
                 text="**Join My Updates Channel to use ME 😎 🤭**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/filmzoneeee")]
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
               ])
             )
-            return
-        except Exception:
-            await update.reply_text("Something Wrong. Contact my Support Group")
             return
     try:
         file_uid = update.command[1]
