e8efdf79dc3ebcfac924a2e1cc0f30654f025013
V-SPOT <83084169+ajmalyaseen@users.noreply.github.com>
2021-05-19 08:14:18 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -224,7 +224,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                photo="https://telegra.ph/file/d39f3e813621a67834758.jpg",
+                photo='https://telegra.ph/file/d39f3e813621a67834758.jpg',
                 text=f"<b>🔎 Got It Your Query 👉 {query}</b>\n\n<b><a href='https://t.me/Film_zone_channels'>©ꜰɪʟᴍ ᴢᴏɴᴇ</a></b>",
                 reply_markup=reply_markup,
                 parse_mode="html",
